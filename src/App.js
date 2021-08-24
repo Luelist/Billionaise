41a5bf575c89ce772a02ebd87f12b129cc5d8802
Luelist <contact@luelist.com>
2021-08-24 02:11:17 +0100

    added list

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				ELECTRIFY
+				<div className="Greeting">
+					① We start, buy, and invest in wonderful internet businesses.
+				</div>
+				<div className="Greeting">② Our family of companies includes:</div>
+				<div className="Greeting">③ We have also invested in:</div>
+				<div className="Greeting" id="sub">
+					④ Interested in seeling us a business?
+					<p>
+						ⓐ <b>We arent your typical buyer</b>
+					</p>
+					<p>ⓑ</p>
+				</div>
 			</header>
+			<main></main>
 		</div>
 	);
 }
